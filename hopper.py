fc4223e625cd52aff37f59dbb456d53b42e198f3
Crissy-Nguyen <tur54776@temple.edu>
2023-11-11 19:15:05 -0500

    Update hopper.py

--- hopper.py
+++ hopper.py
@@ -6,13 +6,27 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Hopper')
 
 player = pygame.Rect((400, 300, 50, 50))
 
 run = True
 while run:
 
+    screen.fill((0, 0, 0))
+
     pygame.draw.rect(screen, (255, 255, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    if key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    if key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    if key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+    
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:

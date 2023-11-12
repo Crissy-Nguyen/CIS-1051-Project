76c66d5297ec1864f4a5bf5cd29ce8fc0ea9d280
Crissy-Nguyen <tur54776@temple.edu>
2023-11-11 19:00:07 -0500

    Update hopper.py

--- hopper.py
+++ hopper.py
@@ -7,9 +7,16 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+player = pygame.Rect((400, 300, 50, 50))
+
 run = True
 while run:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        run = False
+
+    pygame.draw.rect(screen, (255, 255, 0), player)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
 pygame.quit()

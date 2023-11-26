5761ab8984917bff2ec76081b7dd545701c03811
Crissy-Nguyen <tur54776@temple.edu>
2023-11-25 20:50:35 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,18 +4,22 @@ from pygame.locals import *
 
 pygame.init()
 
+# Framerate setup
+clock = pygame.time.Clock()
+FPS = 60
+
 # Game window setup
 screen_width = 576
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('test')
 
-clock = pygame.time.Clock()
-FPS = 60
 
 # Create a single color background
 def draw_bg():
     screen.fill((143, 191, 190))
+
+game_over = False
 
 # Grid setup for measurement purposes
 tile_size = 48
@@ -30,6 +34,8 @@ class Player():
     def __init__(self, x, y):
         img = pygame.image.load('sprites/player/0.png')
         self.image = pygame.transform.scale(img, (32, 32))
+        dead_img = pygame.image.load('sprites/player/1.png')
+        self.dead_image = pygame.transform.scale(dead_img, (32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -40,50 +46,61 @@ class Player():
         self.flip = False
         self.direction = 0
 
-    def update(self):
+    def update(self, game_over):
 
         change_x = 0
         change_y = 0
-        
-        # Key presses
-        key = pygame.key.get_pressed()
-        if key[pygame.K_w] and self.jumped == False:
-            self.vel_y = -12
-            self.jumped = True
-        if key[pygame.K_w] == False:
-            self.jumped = False
-        if key[pygame.K_a]:
-            change_x -= 5
-            self.flip = True
-        if key[pygame.K_d]:
-            change_x += 5
-            self.flip = False
-        
-        # Gravity
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        change_y += self.vel_y
 
-        # Check for collision
-        for t in world.tiles:
-            # In x direction
-            if t[1].colliderect(self.rect.x + change_x, self.rect.y, self.width, self.height):
-                change_x = 0
-            # In y direction
-            if t[1].colliderect(self.rect.x, self.rect.y + change_y, self.width, self.height):
-                # Check if player is below the ground (i.e. jumping)
-                if self.vel_y < 0:
-                    change_y = t[1].bottom - self.rect.top
-                    self.vel_y = 0
-                # Check if player is above the ground (i.e. falling)
-                elif self.vel_y >= 0:
-                    change_y = t[1].top - self.rect.bottom
-                    self.vel_y = 0
+        if game_over == False:
+        
+            # Key presses
+            key = pygame.key.get_pressed()
+            if key[pygame.K_w] and self.jumped == False:
+                self.vel_y = -12
+                self.jumped = True
+            if key[pygame.K_w] == False:
+                self.jumped = False
+            if key[pygame.K_a]:
+                change_x -= 5
+                self.flip = True
+            if key[pygame.K_d]:
+                change_x += 5
+                self.flip = False
+            
+            # Gravity
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            change_y += self.vel_y
 
-        # Update player coords
-        self.rect.x += change_x
-        self.rect.y += change_y
+            # Check for collision
+            for t in world.tiles:
+                # In x direction
+                if t[1].colliderect(self.rect.x + change_x, self.rect.y, self.width, self.height):
+                    change_x = 0
+                # In y direction
+                if t[1].colliderect(self.rect.x, self.rect.y + change_y, self.width, self.height):
+                    # Check if player is below the ground (i.e. jumping)
+                    if self.vel_y < 0:
+                        change_y = t[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    # Check if player is above the ground (i.e. falling)
+                    elif self.vel_y >= 0:
+                        change_y = t[1].top - self.rect.bottom
+                        self.vel_y = 0
+
+            # Check for collision with hazards
+            if pygame.sprite.spritecollide(self, spike_group, False):
+                game_over = True
+                print("You died.")
+            
+            # Update player coords
+            self.rect.x += change_x
+            self.rect.y += change_y
+
+        # Player sprite changes when hitting a hazard
+        elif game_over:
+            self.image = self.dead_image
 
         # Temporary floor
         '''if self.rect.bottom > screen_height-48:
@@ -95,6 +112,9 @@ class Player():
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 0), self.rect, 2)
 
+        return game_over
+
+    # Sprite will visibly switch directions
     def flippy(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
@@ -172,7 +192,7 @@ while run:
     draw_bg()
     world.draw()
     spike_group.draw(screen)
-    player.update()
+    game_over = player.update(game_over)
     player.flippy()
     #draw_grid()
 

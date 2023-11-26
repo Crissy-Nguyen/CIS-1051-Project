77295848b782b05a24fa9d1d74190dc6277c86cc
Crissy-Nguyen <tur54776@temple.edu>
2023-11-25 19:39:56 -0500

    changes.

--- test.py
+++ test.py
@@ -29,10 +29,12 @@ def draw_grid():
 class Player():
     def __init__(self, x, y):
         img = pygame.image.load('sprites/player/0.png')
-        self.image = pygame.transform.scale(img, (tile_size, tile_size))
+        self.image = pygame.transform.scale(img, (32, 32))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
         self.vel_y = 0
         self.jumped = False
         self.flip = False
@@ -64,16 +66,31 @@ class Player():
         change_y += self.vel_y
 
         # Check for collision
+        for t in world.tiles:
+            # In x direction
+            if t[1].colliderect(self.rect.x + change_x, self.rect.y, self.width, self.height):
+                change_x = 0
+            # In y direction
+            if t[1].colliderect(self.rect.x, self.rect.y + change_y, self.width, self.height):
+                # Check if player is below the ground (i.e. jumping)
+                if self.vel_y < 0:
+                    change_y = t[1].bottom - self.rect.top
+                    self.vel_y = 0
+                # Check if player is above the ground (i.e. falling)
+                elif self.vel_y >= 0:
+                    change_y = t[1].top - self.rect.bottom
+                    self.vel_y = 0
 
         # Update player coords
         self.rect.x += change_x
         self.rect.y += change_y
 
         # Temporary floor
-        if self.rect.bottom > screen_height-48:
+        '''if self.rect.bottom > screen_height-48:
             self.rect.bottom = screen_height-48
             change_y = 0
-            
+        '''
+        
         # Draw player onto screen
         screen.blit(self.image, self.rect)
         pygame.draw.rect(screen, (255, 255, 0), self.rect, 2)
@@ -99,37 +116,51 @@ class World():
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    t = (img, img_rect)
-                    self.tiles.append(t)
+                    tile = (img, img_rect)
+                    self.tiles.append(tile)
                 if tile == -1:
                     img = pygame.transform.scale(carrot, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    t = (img, img_rect)
-                    self.tiles.append(t)
+                    tile = (img, img_rect)
+                    self.tiles.append(tile)
                 col_count += 1
+                if tile == 2:
+                    spike = Spike(col_count * tile_size - 39.5, row_count * tile_size + 16)
+                    spike_group.add(spike)
             row_count += 1
 
     def draw(self):
-        for i in self.tiles:
-            screen.blit(i[0], i[1])
+        for t in self.tiles:
+            screen.blit(t[0], t[1])
+            pygame.draw.rect(screen, (255, 0, 255), t[1], 2)
             
+class Spike(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.img = pygame.image.load('sprites/things/2.png')
+        self.image = pygame.transform.scale(self.img, (32, 32))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
 world_data = [
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0], 
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, random.randint(-1,0), 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+[0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 2, 1, 1, 0, 0, 0], 
+[0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0], 
+[0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0], 
+[1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1]
 ]
 
 player = Player(48*2, 48*8)
+
+spike_group = pygame.sprite.Group()
 
 world = World(world_data)
 
@@ -140,6 +171,7 @@ while run:
 
     draw_bg()
     world.draw()
+    spike_group.draw(screen)
     player.update()
     player.flippy()
     #draw_grid()
